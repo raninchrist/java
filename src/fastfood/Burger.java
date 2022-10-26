ae2688a8730f73aeea7bdd5f51d82535580bb697
Ran Shen <98277436+raninchrist@users.noreply.github.com>
2022-10-26 14:22:26 -0700

    burgerArray

--- src/fastfood/Burger.java
+++ src/fastfood/Burger.java
@@ -23,5 +23,5 @@ public class Burger {
     public String getName() {
         return name;
     }
-
+    
 }
--- src/fastfood/FastFood.java
+++ src/fastfood/FastFood.java
@@ -6,7 +6,22 @@ public class FastFood {
         Side s = new Side ("Fries", 1.99, 190);
         Drink d = new Drink ("Coke", 1.69, 95);
         Meal m = new Meal (b, s, d);
-        System.out.format("Your order is %s\nSubtotal: $%5.2f and Calories: %d", 
+        System.out.format("Your order is %s\nSubtotal: $%5.2f and Calories: %d\n", 
             m, m.total(), m.getCal());
+        Burger[] ba = new Burger[10];
+        double subtotal = 0;
+        String[] burgerNames = {"Hamburger", "Cheezeburger", "Double-double"};
+        double[] prices = {1.99, 2.49, 5.09};
+        int[] calories = {190, 300, 450};
+
+        for (int i = 0; i < ba.length; i++ ) { // construction
+            int ri = (int) (Math.random() * 3);
+            ba[i] = new Burger (burgerNames[ri], prices[ri], calories[ri]);
+            subtotal += ba[i].getPrice();
+            System.out.println (ba[i].getName());
+        }
+        System.out.format("Subtotal: %.2f", subtotal);
+
+
     }
 }
